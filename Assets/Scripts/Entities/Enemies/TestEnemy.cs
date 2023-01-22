2428df2be832a113c54574c34ad6cb75b04f9f4d
Alex <alexreal2015@yandex.ru>
2023-01-22 18:32:24 +0500

    Добавил патфайндер, убрал (не удалил) ММ TDE
    MM TDE3 не содержит в себе функций патфайндера, вернулся к A*. Настроил TestEnemy и тестовый Target, все работает.
    1. Враг опять умирает с одной тычки.
    2. Враг постоянно застревает в стенах т.к. пытается через них срезать.

--- Assets/Scripts/Entities/Enemies/TestEnemy.cs
+++ Assets/Scripts/Entities/Enemies/TestEnemy.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class TestEnemy : EnemyClass
 
 {
-    
+    private AIPath aIPath;
+
+    public Transform target;
+
+    private void Awake()
+    {
+        aIPath = GetComponent<AIPath>();
+    }
+
+    private void Update()
+    {
+        aIPath.destination = target.position;
+    }
 }
